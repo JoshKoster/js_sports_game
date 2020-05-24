47935095ebf170a1febeefdd371f2ea8704054fc
JoshKoster <62957883+JoshKoster@users.noreply.github.com>
2020-05-24 14:52:57 -0600

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,64 @@
-//team one
+let teamoneShootButton = document.querySelector('#teamone-shoot-button');
+let teamoneNumshots = document.querySelector('#teamone-numshots');
+let teamoneNumgoals = document.querySelector('#teamone-numgoals');
+let resetButton = document.querySelector('#reset-button');
+let numResets = document.querySelector('#num-resets');
 
-let shotsOne = document.querySelector('#teamone-shoot-button')
-let teamOneShots = document.querySelector('#teamone-numshots')
 
-shotsOne.addEventListener('click', function () {
 
-    let teamOneNum = parseInt(teamOneShots.innerText) + 1
-    teamOneShots.innerText = teamOneNum
-    let random = Math.random() * 100
-    if (random > 50) {
-        document.getElementById("demo").innerHTML = "team scores"
+resetButton.addEventListener("click", function(){
+    console.log("reset button");
+
+    let newNumberRest = Number(numResets.innerHTML) +1;
+    numResets.innerHTML = newNumberRest
+    teamoneNumshots.innerHTML = 0;
+    teamtwoNumberShots.innerHTML = 0;
+    teamoneNumgoals.innerHTML = 0;
+    teamtwoNumgoals.innerHTML = 0;
+})
+
+
+teamoneShootButton.addEventListener("click", function(){
+    console.log(" team one  button clicked");
+
+   let newshotValue = Number(teamoneNumshots.innerHTML) +1; 
+    teamoneNumshots.innerHTML = newshotValue;
+    
+    
+
+
+    if (Math.random() < .3 ){
+        console.log('I did math.random')
+        let newgoalValue= Number(teamoneNumgoals.innerHTML) +1;
+        teamoneNumgoals.innerHTML = newgoalValue;
     }
-    else {
-        document.getElementById("demo").innerHTML = "team missed"
-    }
-
-    let tmOneGoals = document.querySelector('#teamone-numgoals')
-    let tmOneScore = parseInt(tmoneGoals.innerText) + (Math.random)
-    tmOneGoals.innerText = tmOneScore
-
 
 })
-//team two
 
-let shotsTwo = document.querySelector('#teamtwo-shoot-button')
-let teamTwoShots = document.querySelector('#teamtwo-numshots')
 
-shotsTwo.addEventListener('click', function () {
 
-    let teamTwoNum = parseInt(teamTwoShots.innerText) + 1
-    teamTwoShots.innerText = teamTwoNum
-    let random = Math.random() * 100
-    if (random > 50) {
-        document.getElementById("demo").innerHTML = "team scores"
-    }
-    else {
-        document.getElementById("demo").innerHTML = "team missed"
-    }
+let teamtwoShootButton = document.querySelector('#teamtwo-shoot-button');
+let teamtwoNumberShots = document.querySelector('#teamtwo-numbershots');
+let teamtwoNumgoals = document.querySelector('#teamtwo-numgoals');
 
-    let tmTwoGoals = document.querySelector('#teamtwo-numgoals')
-    let tmTwoScore = parseInt(tmTwoGoals.innerText) + (Math.random)
-    tmTwoGoals.innerText = tmTwoScore
 
-})
-// reset button
 
-let reset = document.querySelector('#reset-button')
-let resetNum = document.querySelector('#num-reset')
-reset.addEventListener('click', function () {
-    let startOver = parseInt(resetNum.innerText) + 1
-    resetNum.innerText = startover
+teamtwoShootButton.addEventListener("click", function () {
+   
+    console.log(" team two  button clicked");
 
-})
+  
+    let newshotValue = Number(teamtwoNumberShots.innerHTML) +1; 
+    teamtwoNumberShots.innerHTML = newshotValue;
+
+
+
+   
+    
+      
+        if (Math.random() < .3 ){
+            console.log('I did math.random')
+            let newtwogoalValue= Number(teamtwoNumgoals.innerHTML) +1;
+            teamtwoNumgoals.innerHTML = newtwogoalValue;
+        }
+    })
